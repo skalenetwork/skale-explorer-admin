97b25a6968c2249f010c89a4e145e0f3cd7d6636
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2024-04-23 12:17:11 +0100

    Refactor envs generation

--- admin/core/containers.py
+++ admin/core/containers.py
@@ -1,8 +1,10 @@
 import logging
-import docker
-import socket
-from contextlib import closing
+import os
+import subprocess
 
+import docker
+
+from admin import DOCKER_COMPOSE_BIN_PATH, DOCKER_COMPOSE_CONFIG_PATH
 from admin.utils.logger import init_logger
 
 init_logger()
@@ -11,34 +13,12 @@ dutils = docker.DockerClient()
 
 
 CONTAINER_NOT_FOUND = 'not_found'
-EXITED_STATUS = 'exited'
-CREATED_STATUS = 'created'
 RUNNING_STATUS = 'running'
 
 
-def is_explorer_found(schain_name):
-    container_name = f'{schain_name}_proxy'
-    return is_container_exists(container_name)
-
-
 def is_explorer_running(schain_name):
-    container_name = f'{schain_name}_backend'
-    return get_info(container_name) == RUNNING_STATUS
-
-
-def remove_explorer(schain_name):
     container_name = f'{schain_name}_proxy'
-    if is_container_exists(container_name):
-        logger.warning(f'Removing {container_name}...')
-        return dutils.containers.get(container_name).remove(force=True)
-
-
-def is_container_exists(name: str) -> bool:
-    try:
-        dutils.containers.get(name)
-    except docker.errors.NotFound:
-        return False
-    return True
+    return get_info(container_name) == 'running'
 
 
 def get_info(container_id: str):
@@ -51,49 +31,34 @@ def get_info(container_id: str):
         return CONTAINER_NOT_FOUND
 
 
-def get_db_port(schain_name):
-    try:
-        db = dutils.containers.get(f'{schain_name}_db')
-        return get_container_host_port(db)
-    except docker.errors.NotFound:
-        return get_free_port()
-
-
-def get_container_host_port(container):
-    ports = list(container.attrs['NetworkSettings']['Ports'].values())
-    return ports[0][0]['HostPort']
-
-
 def restart_nginx():
     nginx = dutils.containers.get('nginx')
     logger.info('Restarting nginx container...')
     nginx.exec_run('nginx -s reload')
 
 
-def restart_postgres(schain_name):
-    try:
-        db = dutils.containers.get(f'{schain_name}_db')
-        logger.info(f'Restarting {schain_name}_db container...')
-        db.restart()
-    except docker.errors.NotFound:
-        logger.warning(f'DB for {schain_name} not found')
+def run_blockscout_containers(env_file_path):
+    command = [
+        DOCKER_COMPOSE_BIN_PATH,
+        'compose',
+        '-f',
+        DOCKER_COMPOSE_CONFIG_PATH,
+        '--env-file',
+        env_file_path,
+        'up',
+        '-d'
+    ]
+    subprocess.run(command, env={**os.environ})
 
 
-def get_free_port():
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(('', 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
-
-
-def check_db_exists(schain_name):
-    try:
-        dutils.containers.get(f'{schain_name}_db')
-        return True
-    except docker.errors.NotFound:
-        return False
-
-
-def check_db_running(schain_name):
-    container_name = f'{schain_name}_db'
-    return get_info(container_name) == RUNNING_STATUS
+def stop_blockscout_containers(env_file_path):
+    command = [
+        DOCKER_COMPOSE_BIN_PATH,
+        'compose',
+        '-f',
+        DOCKER_COMPOSE_CONFIG_PATH,
+        '--env-file',
+        env_file_path,
+        'down'
+    ]
+    subprocess.run(command, env={**os.environ})
